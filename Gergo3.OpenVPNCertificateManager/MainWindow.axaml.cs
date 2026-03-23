52ca6e51e967fa03c881e6a6d05b048e6dd8374e
Simon Gergely <simon.gergely2006@gmail.com>
2026-03-23 15:54:54 +0100

    Initial commit

---
+++ Gergo3.OpenVPNCertificateManager/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Gergo3.OpenVPNCertificateManager;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
